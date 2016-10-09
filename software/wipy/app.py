51c0daed403b265748b0f151c3f692d38cb1691e
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-10-09 22:26:46 +0200

    Create a BaseApplication class

---
+++ software/wipy/app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Base application module."""
+
+from grid import Grid
+
+
+class BaseApplication():
+    """Base class for Applications."""
+
+    def __init__(self):
+        """Create a base application."""
+        self._grid = Grid(12, 12)
