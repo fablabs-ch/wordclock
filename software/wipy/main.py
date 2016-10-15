1f8ed103fe43e2ef2a6dace1c1b2893342328694
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-10-15 18:27:35 +0200

    Rename wordclock -> main

--- software/wipy/main.py
+++ software/wipy/main.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
-"""WordClock application module."""
+"""Main application module."""
 
-import time
 from app import BaseApplication
 from meteo import Meteo
 
 
-class WordClock(BaseApplication):
-    """WordClock main application."""
+# pylint: disable=too-few-public-methods
+class Main(BaseApplication):
+    """Main application."""
 
-    def __init__(self, timer, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialize the wordclock."""
-        super(WordClock, self).__init__(*args, **kwargs)
+        super(Main, self).__init__(*args, **kwargs)
         self._app = Meteo()
-        self._timer = timer
 
     def run(self):
         """Run the wordclock."""
-        co = self._app.run()
+        coroutine = self._app.run()
         while True:
             try:
-                co.send(None)
-                time.sleep(1)
+                coroutine.send(None)
                 # time.sleep_ms(self._timer)
             except StopIteration:
                 break
@@ -29,8 +27,8 @@ class WordClock(BaseApplication):
 
 def main():
     """Entry point of the application"""
-    wordclock = WordClock(1000)
-    loop = wordclock.run()
+    app = Main()
+    app.run()
 
 
 if __name__ == '__main__':
